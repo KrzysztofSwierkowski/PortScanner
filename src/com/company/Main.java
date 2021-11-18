5564e7de24c86e5aed1f22c285a90a4f677e1ba4
Swierkowski.K@gmail.com <Krzysiu3285>
2021-11-18 21:36:18 +0100

    finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Type no. port to check:");
-        int port = scanner.nextInt();
-        Portx portx = new Portx();
-        portx.setPort(port);
-        CheckPort checkPort = new CheckPort();
-        checkPort.openPort(portx.getPort());
-
+        while(true)  {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Type no. port to check:");
+            int port = scanner.nextInt();
+            Portx portx = new Portx();
+            portx.setPort(port);
+            CheckPort checkPort = new CheckPort();
+            checkPort.openPort(portx.getPort());
+        }
     }
 }
